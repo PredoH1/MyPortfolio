588b51ba618e7a7ca2e2c0a660213d4ed87f85d6
PredoH1 <pedroh2006candido@gmail.com>
2024-04-24 15:40:05 -0300

    feat: Create About Me page

--- src/components/AboutMe/About.jsx
+++ src/components/AboutMe/About.jsx
@@ -1,31 +1,55 @@
 import React from "react";
+import * as S from "./style";
 import IconMusic from "../../assets/iconMusic.svg";
 import Lamp from "../../assets/lamp.svg";
 import arrowAbout from "../../assets/arrowAbout.svg";
+import htmlIcon from "../../assets/htmlIcon.svg";
+import cssIcon from "../../assets/cssIcon.svg";
+import javascriptIcon from "../../assets/javascriptIcon.svg";
+import reactIcon from "../../assets/react.svg";
 
 export default function About() {
   return (
-    <div>
-      <section>
+    <S.Container>
+      <S.SubContainer>
         <div>
           <img src={IconMusic} alt="" />
         </div>
 
-        <div>
-          <h1>About me</h1>
+        <S.TitleBox>
+          <S.Title>About me</S.Title>
           <img src={Lamp} alt="" />
-        </div>
+        </S.TitleBox>
 
-        <div>
-          <p></p>
-          <a href=""></a>
-        </div>
+        <S.TextaAbout>
+          <S.Paragraph>
+            Olá! Me chamo Pedro Henrique, sou desenvolvedor Front-End com
+            experiência sólida em criar experiências web incríveis. Minha sólida
+            formação técnica e experiência prática me equiparam para enfrentar
+            desafios complexos no desenvolvimento web. Ao longo da minha
+            trajetória, adquiri proficiência nas seguintes tecnologias: HTML ·
+            CSS · JavaScript - React.
+          </S.Paragraph>
+        </S.TextaAbout>
 
-        <div>
+        <S.arrowAbout>
           <img src={arrowAbout} alt="" />
-        </div>
-      </section>
-      <section></section>
-    </div>
+        </S.arrowAbout>
+      </S.SubContainer>
+      <S.ContainerIcon>
+        <S.BoxIcon>
+          <img src={htmlIcon} alt="" />
+        </S.BoxIcon>
+        <S.BoxIcon>
+          <img src={cssIcon} alt="" />
+        </S.BoxIcon>
+        <S.BoxIcon>
+          <img src={javascriptIcon} alt="" />
+        </S.BoxIcon>
+        <S.BoxIcon>
+          <img src={reactIcon} alt="" />
+        </S.BoxIcon>
+      </S.ContainerIcon>
+    </S.Container>
   );
 }
---
+++ src/components/AboutMe/style.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import backgroundImg from "../../assets/imgAbout.svg";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 0.5px solid (238, 238, 238, 0.1);
+`;
+
+export const SubContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
+
+export const TitleBox = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: row;
+  margin: 4.6rem 0rem 0rem 0rem;
+`;
+
+export const Title = styled.h1`
+  color: #eee;
+  font-family: Poppins;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin: 0rem 1.6rem 0rem 0rem;
+`;
+
+export const TextaAbout = styled.section`
+  width: 50%;
+  margin: 1.6rem 0rem 4rem 0rem;
+`;
+
+export const Paragraph = styled.section`
+  color: rgba(238, 238, 238, 0.75);
+  font-family: Poppins;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const arrowAbout = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const ContainerIcon = styled.div`
+  background-image: url(${backgroundImg});
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 35%;
+  height: 25rem;
+`;
+
+export const BoxIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  width: 25%;
+  height: 35%;
+  margin: 0.5rem;
+
+  &:hover {
+    transform: scale(102%);
+  }
+`;
